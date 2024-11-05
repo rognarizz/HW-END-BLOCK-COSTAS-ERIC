ec3fb4e4a3a8156dae674b18a3ccf21d731e4f29
Costas <rgnrz1997@gmail.com>
2024-11-05 14:17:35 +0200

    Создали форму вводу, записываем количество строк длинна которых равна или меньше 3-х

--- Program.cs
+++ Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите элементы массива через пробел:");
+        string[] initialArray = Console.ReadLine().Split(' ');
+
+        int count = 0;
+        for (int i = 0; i < initialArray.Length; i++)
+        {
+            if (initialArray[i].Length <= 3)
+            {
+                count++;
+            };
+        }
+    }
+}
